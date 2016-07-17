4d79d989c5f1ede7ac8678dfb2e57662d232499b
Ian Remsen <ian@ianremsen.com>
2016-07-17 18:56:43 -0400

    uptime

--- bot_modules/main.js
+++ bot_modules/main.js
@@ -108,6 +108,27 @@ module.exports = {
 		}).catch(function(error) {	
 			none_found();
 		});
+	},
+
+	uptime: function(bot, info, words) {
+		String.prototype.toHHMMSS = function () {
+    		var sec_num = parseInt(this, 10); // don't forget the second param
+		    var hours   = Math.floor(sec_num / 3600);
+		    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+		    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+		    // if (hours   < 10) {hours   = "0" + hours;}
+		    // if (minutes < 10) {minutes = "0" + minutes;}
+		    // if (seconds < 10) {seconds = "0" + seconds;}
+		    var time    = hours + ' hours, ' + minutes + ' minutes, and ' + seconds + " seconds.";
+		    return time;
+		}
+
+		var time = process.uptime();
+		var uptime = (time + "").toHHMMSS();
+
+		bot.say(info.args[0], "BotB has been running for " + uptime);
+
 	}
 
 };
