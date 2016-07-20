c526aea889a5da706a31cf92dc95c7ccafcfbb41
Ian Remsen <ian@ianremsen.com>
2016-07-20 16:35:03 -0400

    plural/singular times in up

--- bot_modules/commands.js
+++ bot_modules/commands.js
@@ -267,7 +267,34 @@ module.exports = {
 			// if (hours   < 10) {hours   = "0" + hours;}
 			// if (minutes < 10) {minutes = "0" + minutes;}
 			// if (seconds < 10) {seconds = "0" + seconds;}
-			var time = days + ' days, ' + hours + ' hours, ' + minutes + ' minutes, and ' + seconds + " seconds.";
+			var time = days
+
+			if (days == 1) {
+				time += " day, "
+			} else {
+				time += " days, "
+			}
+
+			time += hours;
+			if (hours == 1) {
+				time += " hour, "
+			} else {
+				time += " hours, "
+			}
+
+			time += minutes;
+			if (minutes == 1) {
+				time += " minute, and "
+			} else {
+				time += " minutes, and "
+			}
+
+			time += seconds;
+			if (seconds == 1) {
+				time += " second. "
+			} else {
+				time += " seconds. "
+			}
 			return time;
 		}
 
