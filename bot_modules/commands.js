6c7d983ce9b656eef744e82591d16d8fe5691711
Ian Remsen <ian@ianremsen.com>
2016-07-20 16:09:45 -0400

    added days to ;up

--- bot_modules/commands.js
+++ bot_modules/commands.js
@@ -252,21 +252,22 @@ module.exports = {
 	},
 
 	uptime: function(bot, info, words) {
-		String.prototype.toHHMMSS = function() {
+		String.prototype.toDDHHMMSS = function() {
 			var sec_num = parseInt(this, 10); // don't forget the second param
-			var hours = Math.floor(sec_num / 3600);
+			var days = Math.floor(sec_num / 86400);
+			var hours = Math.floor((sec_num - (days * 86400)) / 24);
 			var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
 			var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
 			// if (hours   < 10) {hours   = "0" + hours;}
 			// if (minutes < 10) {minutes = "0" + minutes;}
 			// if (seconds < 10) {seconds = "0" + seconds;}
-			var time = hours + ' hours, ' + minutes + ' minutes, and ' + seconds + " seconds.";
+			var time = days + ' days, ' + hours + ' hours, ' + minutes + ' minutes, and ' + seconds + " seconds.";
 			return time;
 		}
 
 		var time = process.uptime();
-		var uptime = (time + "").toHHMMSS();
+		var uptime = (time + "").toDDHHMMSS();
 
 		bot.say(info.channel, "BotB has been running for " + uptime);
 	},
