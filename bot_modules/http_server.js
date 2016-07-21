7197d464a192907a19336238f8ac4a909cde5567
puke7 <knoxburry@gmail.com>
2016-07-20 19:11:41 -0500

    http post server commands closes #12

--- bot_modules/http_server.js
+++ bot_modules/http_server.js
@@ -2,6 +2,15 @@ var config = require('./config.js');
 var http = require('http');
 
 
+var commands = {
+	
+	say: function(bot, message) {
+		bot.say(config.irc.channels[0], message);
+		respond(response, 200, 'text/plain', 'the bot speaks!');
+	},
+
+};
+
 var post_handler = function(request, response) {
 	return new Promise(function(resolve, reject) {
 		var post_data = '';
@@ -22,7 +31,7 @@ var post_handler = function(request, response) {
 
 var respond = function(response, code, content, message) {
 	response.writeHead(code, {'Content-Type':content});
-	response.end(message);
+	response.end(message + '\n');
 };
 
 
@@ -31,25 +40,28 @@ module.exports = {
 	intialize: function(bot) {
 
 		http.createServer(function (request, response) {
-			console.log('SERVER REQUEST :: ' + request.url);
-
-			// XXX need to check for key
-			console.log(request.method);
+			console.log('SERVER REQUEST :: ' + request.method + ' ' + request.url);
 			if (request.method == 'POST') {
 				var p = post_handler(request, response);
 				p.then(function(data) {
 					console.log(data);
-					
-					bot.say(config.irc.channels[0], data.string);
+					// check for valid key
+					if (data.key !== config.http.key) {
+						respond(response, 500, 'text/plain', 'invalid access key');
+						return;
+					}
+					// check for and run command
+					if (typeof commands[data.command] === 'function') {
+						commands[data.command](bot, data.message);
+					}
 				});
 			}
 			else {
 				respond(response, 500, 'text/plain', 'must post data');
 			}
 		}).listen(config.http.port, config.http.ip);
+		// XXX i believe this is firing asynchronously before the server is up
 		console.log('Server running at ' + config.http.ip + ':' + config.http.port);
-
-
 	}
 
 };
