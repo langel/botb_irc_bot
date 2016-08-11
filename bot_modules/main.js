89fbf03fe7e9971d081d886f8e9a7d582a1805df
puke7 <knoxburry@gmail.com>
2016-08-10 21:15:18 -0500

    error message and exit if config.js is not setup

--- bot_modules/main.js
+++ bot_modules/main.js
@@ -2,19 +2,36 @@
 	load all servies and initialize
 */
 
-var services = {
-	http_server: require('./http_server.js'),
-	irc_bot: require('./irc_bot.js'),
-	memory: require('./memory'),
-};
+var fs = require('fs');
+
+var services = [
+	'http_server',
+	'irc_bot',
+	'memory'
+];
+
 
 module.exports = {
 
 	initialize: function() {
-		for (var service_key in services) {
-			console.log('Initializing Module : ' + service_key);
-			services[service_key].initialize();
-		};
+		// check that bot_modules/config.js is setup
+		try {
+			fs.lstatSync('bot_modules/config.js', fs.F_OK);
+			console.log('config.js found');
+			// XXX also need to check all parameters are defined
+			// in the case of config structure updates
+		}
+		catch(e) {
+			console.log('config.js missing from bot_modules/');
+			console.log('copy from bot_modules/config_example.js and edit appropriately');
+			process.exit(1);
+		}
+
+		// initialize services
+		services.forEach(function(service) {
+			var module = require('./' + service + '.js');
+			module.initialize();
+		});
 	},
 
 };
