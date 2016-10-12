langel/botb_irc_bot

commit 92d16ba7ec61781780550641e8fcac4ebe431937

Handle multiple sharps/flats in ultrachord
More than two is madness, so don't allow that. Resolves #42.

bot_modules/irc_ultrachord.js
@@ -62,30 +62,27 @@ module.exports = {
 				return;
 			}
 
-			// the max length of a note is X[#/b][0-9] == 3
-			// anything more is garbage (because we already handled
-			// timbres above)
-			if (param.length > 3) return;
-
-			// if there's 3 characters and the middle is a number,
-			// discard the ultrachord!
-			if (param.length == 3 && (Number.isInteger(parseInt(param.charAt(1), 10)))) return;
+			// abort if a note parameter is not well-formed
+			if (!param.match(/^[a-gA-G](-|[#b♯♭]{0,2})[0-9]$/)) return;
 
 			// note to number function call happens
 			var note_val = noteToNumber(param.charAt(0));
-			// if not a note (A-G)
-			if (note_val == -1) return;
 			filename += param.charAt(0);
 
 			// sharps and flats handling
-			switch (param.charAt(1)) {
-				case '#':
-					note_val++;
-					filename += 'S';
-					break;
-				case 'b':
-					note_val--;
-					filename += 'b';
+			for (var i = 1; i < param.length; i++) {
+				switch (param.charAt(i)) {
+					case '#':
+					case '♯':
+						note_val++;
+						filename += 'S';
+						break;
+					case 'b':
+					case '♭':
+						note_val--;
+						filename += 'b';
+						break;
+				}
 			}
 
 			// octave handling

commit 5cf453a2dd86249f5385881d63a2b229dcb7bfd8

Allow and document default ultrachord octave
Fixes #43, and a bug introduced in 92d16ba.

bot_modules/irc_commands.js
@@ -4,6 +4,7 @@ var config = require('./config.js');
 var kudos = require('./irc_kudos.js');
 var memory = require('./memory.js');
 var request = require('request');
+var ultrachord = require('./irc_ultrachord.js');
 var util = require('./util.js');
 
 module.exports = {
@@ -170,7 +171,7 @@ module.exports = {
 			levelup: "Usage: " + prefix + "levelup <botbr> | Returns BotBr's current level, current points, calculated points per year, estimated time to level up, estimated time to reach GRAND WIZARD STATUS of level 33, current boons, and calculated boons per year.",
 			pix: "Usage: " + prefix + "pix <botbr> | Returns a URL of a picture of the BotBr in the flesh, if one has been submitted.",
 			top: "Usage: " + prefix + "top [class] | Returns list of top BotBrs over all or by class.",
-			ultrachord: "Usage: " + prefix + "ultrachord <notes> [timbre] | Returns a URL to a .wav file of the notes and timbre provided, in a format such as 'C4 E4 G4 sawtooth'. Available notes range from C0 to B7. If number is omitted it will pick octave 2. Default timbre is sine. Available timbres are sine, sawtooth, square, triangle, and pluck.",
+			ultrachord: "Usage: " + prefix + "ultrachord <notes> [timbre] | Returns a URL to a .wav file of the notes and timbre provided, in a format such as 'C4 E4 G4 sawtooth'. Available notes range from C0 to B7. If number is omitted it will pick octave " + ultrachord.default_octave + ". Default timbre is sine. Available timbres are sine, sawtooth, square, triangle, and pluck.",
 			uptime: "Usage: " + prefix + "uptime | Displays how long the bot has been running.",
 			wikipedia: "Usage: " + prefix + "wikipedia <query> | Returns a URL of the Wikipedia search of your query.",
 			youtube: "Usage: " + prefix + "youtube <query> | Returns a URL of the YouTube search of your query.",
@@ -497,7 +498,6 @@ module.exports = {
 	 *
 	 */
 	ultrachord: function(info, words) {
-		var ultrachord = require('./irc_ultrachord.js');
 		var chat_text = ultrachord.ultrachord(words);
 		if (chat_text != null) {
 			bot.say(info.channel, info.nick + ": " + chat_text);

bot_modules/irc_ultrachord.js
@@ -1,4 +1,6 @@
 module.exports = {
+	default_octave: 4, // exported so that help can reference it
+
 	// returns a link to an ultrachord
 	ultrachord: function(words) {
 		const execSync = require('child_process').execSync;
@@ -16,7 +18,7 @@ module.exports = {
 		function getFrequency(octave, note) {
 			// 1.059463094359 == 2^12, used for note frequency calculation!
 			var base_note = 10; // A
-			var base_octave = 4; // 4
+			var base_octave = module.exports.default_octave;
 			var note_distance = ((octave * 12) + note) - ((base_octave * 12) + base_note);
 			// 440hz == A4
 			var frequency = 440 * Math.pow(1.059463094359, note_distance);
@@ -63,7 +65,7 @@ module.exports = {
 			}
 
 			// abort if a note parameter is not well-formed
-			if (!param.match(/^[a-gA-G](-|[#b♯♭]{0,2})[0-9]$/)) return;
+			if (!param.match(/^[a-gA-G](-|[#b♯♭]{0,2})[0-9]?$/)) return;
 
 			// note to number function call happens
 			var note_val = noteToNumber(param.charAt(0));